6703543842660f7159ea12d90d4a7cad31a8c531
Rahul Senna <rahulvsenna@gmail.com>
2026-03-23 19:44:23 +0530

    5: Write a tree object

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <zlib.h>
 #include <ctype.h>
 #include <openssl/sha.h>
+#include <dirent.h>
 
 void hexdump(const void* data, size_t size)
 {
@@ -79,6 +80,125 @@ int get_uncompressed(const char* object_id, uint8_t *output, size_t *out_len)
   return 0;
 }
 
+void hash_to_str(uint8_t* hash, char* out)
+{
+  for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+    sprintf(out + i * 2, "%02x", hash[i]);
+}
+void print_hash(uint8_t* hash)
+{
+  if (hash == 0) return;
+  for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
+    printf("%02x", hash[i]);
+  printf("\n");
+}
+
+uint8_t* write_object(uint8_t* raw, size_t raw_len)
+{
+  uLongf compressed_len = compressBound(raw_len);
+  uint8_t* compressed = (uint8_t*) malloc(compressed_len);
+
+  if (compress2(compressed, &compressed_len, (const Bytef*) raw, (uLong) raw_len, Z_DEFAULT_COMPRESSION) != Z_OK)
+  {
+    free(compressed);
+    return 0;
+  }
+
+  uint8_t* id = (uint8_t*) malloc(SHA_DIGEST_LENGTH);
+  SHA1(raw, raw_len, id);
+
+  char hash_str[SHA_DIGEST_LENGTH * 2 + 1];
+  hash_to_str(id, hash_str);
+
+  char dir_path[256];
+  snprintf(dir_path, sizeof(dir_path), ".git/objects/%.*s", 2, hash_str);
+  mkdir(dir_path, 0755);
+  char file_path[256];
+  snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, hash_str + 2);
+
+  FILE* fd = fopen(file_path, "wb");
+  size_t bytes_written = fwrite(compressed, sizeof(uint8_t), compressed_len, fd);
+  fclose(fd);
+
+  free(compressed);
+  return id;
+}
+
+uint8_t* create_object(char *file_name)
+{
+  uint8_t* hash = 0;
+  FILE* fd = fopen(file_name, "rb");
+  fseek(fd, 0, SEEK_END);
+  size_t file_size = ftell(fd);
+  fseek(fd, 0, SEEK_SET);
+
+  char header[64];
+  size_t header_len = snprintf(header, sizeof(header), "blob %zu", file_size);
+  size_t blob_size = file_size + header_len + 1;
+  uint8_t* blob = (uint8_t*) malloc(blob_size);
+  memcpy(blob, header, header_len + 1);
+  size_t bytes_read = fread(blob + header_len + 1, sizeof(uint8_t), file_size, fd);
+  fclose(fd);
+
+  hash = write_object(blob, blob_size);
+  free(blob);
+  return hash;
+}
+
+uint8_t* create_tree(char *root_path)
+{
+  uint8_t* data = (uint8_t*) malloc(1024 * 200);
+  uint8_t* cursor = data;
+  struct dirent** entries;
+  int n = scandir(root_path, &entries, NULL, alphasort);
+  for (int i = 0; i < n; ++i)
+  {
+    struct dirent* entry = entries[i];
+    if (entry->d_name[0] == '.')
+    {
+      free(entries[i]);
+      continue;
+    }
+
+    char path[256];
+    snprintf(path, sizeof(path), "%s/%s", root_path, entry->d_name);
+    uint8_t* hash = 0;
+    if (entry->d_type == DT_DIR)
+    {
+      cursor += snprintf(cursor, 256, "40000 %s", entry->d_name) + 1;
+      hash = create_tree(path);
+    } else if (entry->d_type == DT_REG)
+    {
+      cursor += snprintf(cursor, 256, "100644 %s", entry->d_name) + 1;
+      hash = create_object(path);
+    }
+    if (hash)
+    {
+      memcpy(cursor, hash, SHA_DIGEST_LENGTH);
+      cursor += SHA_DIGEST_LENGTH;
+      free(hash);
+    }
+    free(entries[i]);
+  }
+
+  size_t data_len = cursor - data;
+  char tree_header[64];
+  size_t tree_header_len = snprintf(tree_header, 64, "tree %zu", data_len);
+  size_t tree_data_len = data_len + tree_header_len + 1;
+
+  uint8_t* tree_data = (uint8_t*) malloc(tree_data_len);
+  memcpy(tree_data, tree_header, tree_header_len + 1);
+  memcpy(tree_data + tree_header_len + 1, data, data_len);
+
+  uint8_t* tree_id = write_object(tree_data, tree_data_len);
+
+  free(entries);
+  free(data);
+  free(tree_data);
+  return tree_id;
+}
+
+
 int main(int argc, char* argv[])
 {
   // Disable output buffering
@@ -131,47 +251,7 @@ int main(int argc, char* argv[])
   }
   else if (strcmp(command, "hash-object") == 0)
   {
-    char* file_name = argv[3];
-    FILE* fd = fopen(file_name, "rb");
-    fseek(fd, 0, SEEK_END);
-    size_t file_size = ftell(fd);
-    fseek(fd, 0, SEEK_SET);
-
-    char header[64];
-    size_t header_len = snprintf(header, sizeof(header), "blob %zu", file_size);
-    size_t blob_size = file_size + header_len + 1;
-    uint8_t* blob = (uint8_t*) malloc(blob_size);
-    memcpy(blob, header, header_len + 1);
-    size_t bytes_read = fread(blob + header_len + 1, sizeof(uint8_t), file_size, fd);
-    fclose(fd);
-
-    uLongf compressed_len = compressBound(blob_size);
-    uint8_t* compressed = (uint8_t*) malloc(compressed_len);
-    int ret = compress2(compressed, &compressed_len, (const Bytef*) blob, (uLong) blob_size, Z_DEFAULT_COMPRESSION);
-    if (ret == Z_OK)
-    {
-      unsigned char hash[SHA_DIGEST_LENGTH];
-      SHA1(blob, blob_size, hash);
-
-      char hash_str[SHA_DIGEST_LENGTH * 2 + 1];
-      for (int i = 0; i < SHA_DIGEST_LENGTH; i++)
-        sprintf(hash_str + i * 2, "%02x", hash[i]);
-
-      printf("%s\n", hash_str);
-
-      char dir_path[256];
-      snprintf(dir_path, sizeof(dir_path), ".git/objects/%.*s", 2, hash_str);
-      mkdir(dir_path, 0755);
-      char file_path[256];
-      snprintf(file_path, sizeof(file_path), "%s/%s", dir_path, hash_str + 2);
-
-
-      FILE* fd = fopen(file_path, "wb");;
-      size_t bytes_written = fwrite(compressed, sizeof(uint8_t), compressed_len, fd);
-      fclose(fd);
-    }
-    free(blob);
-    free(compressed);
+    print_hash(create_object(argv[3]));
   }
   else if (strcmp(command, "ls-tree") == 0)
   {
@@ -192,6 +272,10 @@ int main(int argc, char* argv[])
       printf("%s\n", name);
     }
     free(data);
+  }
+  else if (strcmp(command, "write-tree") == 0)
+  {
+    print_hash(create_tree("."));
   }
   else
   {
